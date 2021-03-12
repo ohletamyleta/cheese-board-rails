26c90ef8fb5b1988496cc3ee68d84bc9048514a2
ohletamyleta <ohletamyleta@gmail.com>
2021-03-12 15:36:45 -0500

    build review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :cheese
+  belongs_to :user 
+
+  validates :title, presence: true 
+  validates :blocks, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 11}
+
+  validates :cheese, uniqueness: { scope: :user, message: "has already been reviewed by you" }
+
+
+
 end
