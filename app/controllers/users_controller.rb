aaa2c31d26844a0e21eddcb4eb90039b458e2a37
ohletamyleta <ohletamyleta@gmail.com>
2021-03-12 15:40:44 -0500

    build users controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+
+  skip_before_action :redirect_if_not_logged_in, only: [:new, :create]
+  
+
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save 
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else 
+      render :new
+    end
+  end 
+
+  def show 
+    @user = User.find_by(id: params[:id])
+    redirect_to '/' if !@user
+  end 
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end 
+
 end
