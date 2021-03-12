907fb16dfbb77510c59143cacab51d2bae683728
ohletamyleta <ohletamyleta@gmail.com>
2021-03-12 18:57:40 -0500

    created wine resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  
+  resources :wines
   root 'sessions#index'
  
  get '/login' => 'sessions#new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_202156) do
+ActiveRecord::Schema.define(version: 2021_03_12_235530) do
 
   create_table "cheeses", force: :cascade do |t|
     t.string "name"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 2021_03_12_202156) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "wines", force: :cascade do |t|
+    t.string "varietal"
+    t.string "region"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
