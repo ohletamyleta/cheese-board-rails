a04e611b2ee004bd039bdb002db6688ab88f9f00
ohletamyleta <ohletamyleta@gmail.com>
2021-03-12 16:40:44 -0500

    build cheese controller new/create methods

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,2 +1,23 @@
 class CheesesController < ApplicationController
+
+  def new
+  
+  end
+
+  def create 
+
+  end 
+
+  def show 
+  
+  end 
+
+  def edit 
+  end 
+
+  def update 
+  end 
+
+
+  
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :cheese
   belongs_to :user 
 
+
   validates :title, presence: true 
   validates :blocks, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 11}
 
