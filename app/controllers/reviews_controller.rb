81f4f3933cf53d78dcdb5f1c6f4e390a47242c10
ohletamyleta <ohletamyleta@gmail.com>
2021-03-12 17:54:55 -0500

    build reviews controller actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,24 @@
 class ReviewsController < ApplicationController
   before_action :redirect_if_not_logged_in
 
-  
+  def new
+  end
+
+  def create 
+  end
+
+  def show 
+  end 
+
+  def index 
+  end
 
 
+  private 
 
-
+  def review_params
+    params.require(:review).permit(:cheese_id, :title, :blocks, :content)
+  end  
 
 
 end
