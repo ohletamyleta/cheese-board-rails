a1b756dfbd74f15ce23679f4eabed4a6082dcd74
ohletamyleta <ohletamyleta@gmail.com>
2021-03-12 17:35:17 -0500

    add methods & action details to cheese controller

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,5 +1,9 @@
 class CheesesController < ApplicationController
 
+  before_action :set_cheese, only:[:show, :edit, :update]
+  
+
+
   def new
     @cheese = Cheese.new
     @cheese.build_style
@@ -7,11 +11,17 @@ class CheesesController < ApplicationController
   end
 
   def create 
-
+    @cheese = Cheese.new(cheese_params)
+    @cheese.user_id = session[:user_id]
+    @cheese.build_style if !@cheese.style
+    if @cheese.save 
+      redirect_to cheese_path(@cheese)
+    else
+    render :new
+    end 
   end 
 
   def show 
-  
   end 
 
   def edit 
@@ -20,6 +30,15 @@ class CheesesController < ApplicationController
   def update 
   end 
 
+private
 
+  def cheese_params
+    params.require(:cheese).permit(:name, :color, :texture, :notes, :style_id, style_attributes:[:name, :details])
+  end
+
+  def set_cheese
+    @cheese = Cheese.find_by(params[:id])
+    redirect_to cheese_path if !cheese 
+  end
 
 end
