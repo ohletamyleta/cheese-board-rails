fc96f6db6ddd67744dc942540de98915e49af854
ohletamyleta <ohletamyleta@gmail.com>
2021-03-12 15:48:06 -0500

    add routes for login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+ 
+  root 'sessions#index'
+ 
+ get '/login' => 'sessions#new'
+ post '/login' => 'sessions#create'
+ get '/logout' => 'sessions#destroy'
+
+ get '/signup' => 'users#new'
+ post '/signup' => 'users#create'
+ 
+ 
+ 
+ 
+ 
   resources :reviews
   resources :styles
   resources :cheeses
