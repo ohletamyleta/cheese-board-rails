e49caf92d768136171c7245643e9f1098ac23480
ohletamyleta <ohletamyleta@gmail.com>
2021-03-12 15:29:30 -0500

    build user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   has_many :cheeses
   has_many :reviewed_cheeses, through: :reviews, source: :cheese
 
-  
+  validates :name, :email, presence: true 
+  validates :name, :email, uniqueness: true
+  has_secure_password
 end
