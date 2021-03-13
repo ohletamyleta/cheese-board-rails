a9a107091d99b4b89705b7eeb0793bb41f302ce8
ohletamyleta <ohletamyleta@gmail.com>
2021-03-12 19:01:17 -0500

    add model for wine

--- app/models/style.rb
+++ app/models/style.rb
@@ -8,3 +8,4 @@ class Style < ApplicationRecord
 
   
 end
+
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,2 +1,9 @@
 class Wine < ApplicationRecord
+
+  has_many :cheeses
+
+  validates :varietal, uniqueness: true 
+
+  scope :order_alpha, -> { order(name: :asc)}
+  
 end
