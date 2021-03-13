5e407b6ec0ebaaf02bec85c1cfbdbc0bf8834dfb
ohletamyleta <ohletamyleta@gmail.com>
2021-03-13 12:57:47 -0500

    add helper method for review views

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,4 +1,10 @@
 module ReviewsHelper
-
   
+  def display_header(review)
+    if params[:cheese_id]
+        content_tag(:h1, "Add a Review for #{review.cheese.name} -  #{review.cheese.style.name}")
+    else
+      content_tag(:h1, "Create a review")
+    end
+  end
 end
