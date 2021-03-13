34d0a714a76efcfaf7f90e3185495296fcfb285e
ohletamyleta <ohletamyleta@gmail.com>
2021-03-13 12:38:52 -0500

    build new & create actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,9 +2,20 @@ class ReviewsController < ApplicationController
   before_action :redirect_if_not_logged_in
 
   def new
+    if @cheese = Cheese.find_by_id(params[:cheese_id])
+      @review = @cheese.reviews.build
+    else
+      @review = Review.new
+    end 
   end
 
   def create 
+    @review = current_user.reviews.build(reviews_params)
+    if @review.save
+      redirect_to review_path(@review)
+    else
+      render :new
+    end 
   end
 
   def show 
