aa67277b05ff730f3cca25abb5e09dd830797838
ohletamyleta <ohletamyleta@gmail.com>
2021-03-18 13:29:20 -0400

    refactor and add nested routing

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,7 +6,7 @@ class Review < ApplicationRecord
   validates :title, presence: true 
   validates :blocks, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 11}
 
-  validates :cheese, uniqueness: { scope: :user, message: "has already been reviewed by you" }
+
 
   scope :top_cheeses, -> { where('blocks > ?', 8) }
 
